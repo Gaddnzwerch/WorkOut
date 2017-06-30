9e16afd5fa14f3bab03e35ddeab7702ead2528c5
Andreas Schultz <a.schultz@koppermann.com>
2017-06-30 11:04:59 +0200

    added a vibration effect to the countdown

--- app/src/main/java/de/gaddenzwerch/workout/CountdownTimer.java
+++ app/src/main/java/de/gaddenzwerch/workout/CountdownTimer.java
@@ -1,6 +1,7 @@
 package de.gaddenzwerch.workout;
 
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,17 +11,38 @@ public class CountdownTimer extends AppCompatActivity {
 
     private Integer mTimeToCount;
     private TextView mTvTime;
+    private CountDownTimer mCountDownTimer;
+    private Vibrator mVibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown_timer);
 
+        mVibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
         Intent intent = getIntent();
         mTimeToCount = intent.getIntExtra(MainActivity.EXTRA_TIME, 0);
 
         mTvTime = (TextView) findViewById(R.id.tvTime);
         mTvTime.setText(mTimeToCount.toString());
+
+        mCountDownTimer = new CountDownTimer(this.mTimeToCount * 1000, 1000) {
+
+            public void onTick(long pMillisecondsUntilFinished) {
+                Long lSecondsUntilFinished = pMillisecondsUntilFinished / 1000;
+                mTvTime.setText(lSecondsUntilFinished.toString());
+
+                if (lSecondsUntilFinished == 2) {
+                    notifyCountDown();
+                }
+            }
+
+            public void onFinish() {
+                mTvTime.setText("");
+                finish();
+            }
+        };
     }
 
     @Override
@@ -30,18 +52,10 @@ public class CountdownTimer extends AppCompatActivity {
     }
 
     private void startTimer() {
-        new CountDownTimer(this.mTimeToCount * 1000, 1000){
+        mCountDownTimer.start();
+    }
 
-            public void onTick(long pMillisecondsUntilFinished){
-                Long lSecondsLeft = (pMillisecondsUntilFinished + 1000) / 1000;
-                mTvTime.setText(lSecondsLeft.toString());
-            }
-
-            public void onFinish(){
-                mTvTime.setText("");
-                finish();
-            }
-
-        }.start();
+    private void notifyCountDown() {
+        mVibrator.vibrate((long) 100);
     }
 }
